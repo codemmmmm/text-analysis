710f297e91a22f4b1533fc992c55db278a921e39
Moritz <moritz.mueller2@tu-dresden.de>
2022-02-17 17:28:14 +0100

    added table creation and some debug printing

--- scrape.py
+++ scrape.py
@@ -21,6 +21,36 @@ def create_connection(db_file):
 
     return conn
 
+def create_table(conn, create_table_sql):
+    """ create a table from the create_table_sql statement
+    :param conn: Connection object
+    :param create_table_sql: a CREATE TABLE statement
+    :return:
+    """
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print(e)
+
+def create_tables(conn):
+    sql_create_posts_table = '''CREATE TABLE IF NOT EXISTS posts (
+                                        post_id integer PRIMARY KEY,
+                                        post_title text NOT NULL,
+                                        post_body text,
+                                        post_reddit_id text UNIQUE NOT NULL
+                                    ); '''
+
+    sql_create_replies_table = '''CREATE TABLE IF NOT EXISTS replies (
+                                        reply_id integer PRIMARY KEY,
+                                        reply_body text NOT NULL,
+                                        reply_reddit_id text UNIQUE NOT NULL,
+                                        post_id integer NOT NULL,
+                                        FOREIGN KEY (post_id) REFERENCES posts(post_id)
+                                    ); '''                                  
+    create_table(conn, sql_create_posts_table)
+    create_table(conn, sql_create_replies_table)
+
 def insert_post(post):
     # return post_id of inserted row
     sql = '''INSERT INTO posts(post_title, post_body, post_reddit_id)
@@ -35,7 +65,9 @@ def insert_comment(reply):
     cur = conn.cursor()
     cur.execute(sql, reply)
 
-# load .env variables
+DEBUG = True
+
+# load .env file variables
 load_dotenv()
 
 reddit = praw.Reddit(
@@ -48,21 +80,26 @@ reddit = praw.Reddit(
 
 database = 'db/data.db'
 conn = create_connection(database)
+if not DEBUG:
+    create_tables(conn)
 
 min_comments = 30 # includes whole comment forest
 max_comments = 150 # top level comments only
 
 try:
     for post in reddit.subreddit('history').top(limit=6000):
-        print(post)
-        #continue
+        if DEBUG:
+            print(post)
         if not post.stickied and post.num_comments >= min_comments:
             # either commits or rolls back
             with conn: 
                 try:
                     post_id = insert_post((post.title, post.selftext, post.id))
+                    if DEBUG:
+                        print('Inserted post')
                 except sqlite3.IntegrityError:
-                    print("Post already exists in database.")
+                    if DEBUG:
+                        print("Post already exists in database.")
                     continue
             
                 post.comment_sort = 'top'
@@ -77,25 +114,11 @@ try:
                         #if top_level_comment.score < 0:
                         #    break
                         insert_comment((top_level_comment.body, top_level_comment.id, post_id))
+                    if DEBUG:
+                        print('Inserted comments')
     conn.close()     
 except KeyboardInterrupt:
     conn.rollback()
     conn.close()         
     print("Quit by user")  
-
-# select posts that don't have any reply
-# SELECT COUNT(*) FROM posts WHERE 't3_' || post_reddit_id not in (SELECT reply_parent FROM replies);
-# select replies that aren't related to any post
-# SELECT COUNT(*) FROM replies WHERE reply_parent not in (SELECT 't3_' || post_reddit_id FROM posts);
-
-
-
-#posts_panda = []
-#comments_panda = []   
-#comments_panda.append([top_level_comment.id, top_level_comment.body])# ,top_level_comment.link_id])
-#posts_panda.append([post.id, post.title, post.selftext, post.num_comments])        
-#posts_panda = pd.DataFrame(posts_panda,columns=['id', 'title', 'body', 'comments'])
-#comments_panda = pd.DataFrame(comments_panda, columns=['id', 'body',])# 'link_id'])     
-#print(posts_panda)
-#print(comments_panda)
 
