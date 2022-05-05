7a2fe4c32bc75a868587f0fc484b3dfe0176f135
Moritz <moritz.mueller2@tu-dresden.de>
2022-05-05 14:24:13 +0200

    fixed word_count func, changed scoring, ...

--- extract_summary.py
+++ extract_summary.py
@@ -6,6 +6,7 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk.stem import PorterStemmer
 import re
+from rouge_score import rouge_scorer
 
 def create_connection(db_file):
     """ create a database connection to the SQLite database
@@ -32,19 +33,19 @@ def prepare_text(text):
     return re.sub(r'([a-zA-Z"):/])\n', r'\1.\n', text) # or maybe ([^.!?\n])(\n)
 
 def count_words(sentence, filter_stopwords=True):
-    sentence = re.sub('[^a-zA-Z]', ' ', sentence)
-    sentence_list = sentence.strip()
+    sentence = re.sub(r'[^a-zA-Z\']', ' ', sentence) # also ignore underscore?
+    word_list = sentence.split()
     if not filter_stopwords:
-        return len(sentence_list)
+        return len(word_list) 
     else:
         stopword_count = 0
-        for word in sentence_list:
+        for word in word_list:
             if word.lower() in stopwords:
                 stopword_count += 1
-        return len(sentence_list) - stopword_count
+        return len(word_list) - stopword_count # can return 0
 
 def count_frequencies(cleaned_text):
-    """Returns dict for each word, except stopwords, and the normalized frequency.
+    """Returns dict for every word (stemmed), except stopwords, and the normalized frequency.
     """
     frequencies = dict()
     for word in word_tokenize(cleaned_text):
@@ -82,42 +83,56 @@ post_data = cur.fetchone()
 post_text = post_data[0] + '\n' + post_data[1]
 conn.close()
 
-# replace everything except letters for counting frequency
-cleaned_text = re.sub('[^a-zA-Z]', ' ', text)
 
-text = prepare_text(text)
-stemmer = PorterStemmer()
+with open('cnn_dm_articles.txt', 'r') as f:
+    text_list = f.readlines()
+    #print(text)
+
 stopwords = set(stopwords.words("english"))
+summaries = []
+for text in text_list:
+    # replace everything except letters for counting frequency
+    cleaned_text = re.sub('[^a-zA-Z]', ' ', text)
 
-frequencies = count_frequencies(cleaned_text)
+    text = prepare_text(text)
+    stemmer = PorterStemmer()    
 
-# sentence scores
-sentences = sent_tokenize(text)
-sentence_scores = dict()
-for sentence in sentences:
-    # turn sentence into list of stemmed words
-    stemmed_sentence = [stemmer.stem(token, to_lowercase=True) for token in word_tokenize(sentence)]    
-    # score = 0
-    for word, freq in frequencies.items():        
-        if word in stemmed_sentence: # e.g. 'guy' will not return true for ['guys', ], which should be fine because stemmed?
-            if sentence in sentence_scores:
-                sentence_scores[sentence] += freq
-            else:
-                sentence_scores[sentence] = freq
-    if sentence in sentence_scores:
-        score = sentence_scores[sentence]
-        count = count_words(sentence)
-        #count = len(sentence.strip())
-        #print(sentence, str(score) , str(count))            
-        sentence_scores[sentence] = score + 2 * (score / count)
-        #print(sentence, sentence_scores[sentence], (len(sentence.split())), end='\n\n')            
-#for key, value in sorted(sentence_scores.items(), key=lambda item: item[1]):
-#    print(key, value) 
-#print(sentence_scores.items())
+    frequencies = count_frequencies(cleaned_text)
 
-n = 3
-# might throw an error if there happens to be the same sentence twice
-summary_sentences = [x[0] for x in sorted(sentence_scores.items(), reverse=True, key=lambda item: item[1])]
-summary = '\n'.join(summary_sentences[:n])
-#print(summary)
+    # sentence scores
+    sentences = sent_tokenize(text)
+    sentence_scores = dict()
+    for sentence in sentences:
+        # turn sentence into list of stemmed words
+        stemmed_sentence = [stemmer.stem(token, to_lowercase=True) for token in word_tokenize(sentence)]    
+        # score = 0
+        for word, freq in frequencies.items():        
+            if word in stemmed_sentence: # e.g. 'guy' will not return true for ['guys', ], which should be fine because stemmed?
+                if sentence in sentence_scores:
+                    sentence_scores[sentence] += freq
+                else:
+                    sentence_scores[sentence] = freq
+        if sentence in sentence_scores:     
+            score = sentence_scores[sentence]
+            count = count_words(sentence)
+            if count > 0:
+                #count = len(sentence.strip())
+                #print(sentence, str(score) , str(count))            
+                sentence_scores[sentence] = score + 2 * (score / count)
+                #print(sentence, sentence_scores[sentence], (len(sentence.split())), end='\n\n')            
+    #for key, value in sorted(sentence_scores.items(), key=lambda item: item[1]):
+    #    print(key, value) 
+    #print(sentence_scores.items())
+    
+    n = 2
+    # might throw an error if there happens to be the same sentence twice
+    summary_sentences = [x[0] for x in sorted(sentence_scores.items(), reverse=True, key=lambda item: item[1])]
+    summary = ' '.join(summary_sentences[:n])
+    #print(summary)
+    summaries.append(summary + '\n')
+    #scorer = rouge_scorer.RougeScorer(['rouge1', 'rougeL'], use_stemmer=True)
+    #print(scorer.score(reference, summary))
+
+with open('summary-explorer/text-processing/data/raw_files/sumex.txt', 'w') as f:
+    f.writelines(summaries)    
 
